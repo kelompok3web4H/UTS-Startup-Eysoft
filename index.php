b7d61bcf506defbfb50de4b017cbb98270cb0574
Aris Muhandisin <arismuhandisin@webmail.umm.ac.id>
2021-04-21 21:21:42 +0700

    initial commit

--- index.php
+++ index.php
@@ -396,8 +396,7 @@
 
 
     <!-- jQuery -->
-    <script src="assets/js/jquery-2.1.0.min.js"></script>
-
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
     <!-- Bootstrap -->
     <script src="assets/js/popper.js"></script>
     <script src="assets/js/bootstrap.min.js"></script>
